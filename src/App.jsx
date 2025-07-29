92637726891c628e1e38cabb59a07288bdd80505
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-07-29 18:12:08 +0300

    feat: added Hero section with the appropriate styling and interactivity.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
     return (
-        <div className="bg-black">
+        <div>
             <Header />
+            <Hero />
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,45 @@ export default {
             fontFamily: {
                 sans: ["Inter", "Poppins", "Roboto", "sans-serif"],
             },
+            backgroundImage: {
+                nameGradient: "linear-gradient(to right, #345CEB, #7040EC)",
+            },
             colors: {
                 h1: "#524EEC",
+                previewBtn: "#E6F1FF",
             },
             animation: {
                 spin360: "spin360 0.7s ease-in-out",
+                slideInLeft: "slideInLeft 1s ease-in-out",
+                slideInRight: "slideInRight 1s ease-in-out",
             },
             keyframes: {
                 spin360: {
                     "0%": { transform: "rotate(0deg)" },
                     "100%": { transform: "rotate(360deg)" },
                 },
+                slideInLeft: {
+                    "0%": {
+                        transform: "translateX(-100%)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "translateX(0)",
+                        opacity: "1",
+                    },
+                },
+                slideInRight: {
+                    "0%": {
+                        transform: "translateX(200%)",
+                        opacity: "0",
+                    },
+                    "100%": {
+                        transform: "translateX(0)",
+                        opacity: "1",
+                    },
+                },
             },
         },
     },
-    plugins: [],
+    plugins: [import("tailwindcss-animate")],
 };
