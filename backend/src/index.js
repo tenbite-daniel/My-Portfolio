a230996b861298543636510442a6f36c4f888fe1
Tenbite Daniel <101597746+tenbite-daniel@users.noreply.github.com>
2025-08-01 02:40:52 +0300

    Update: added the correct frontend link

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigin =
-    process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL
-        : "http://localhost:5173";
+const allowedOrigin = process.env.FRONTEND_URL;
 
 app.use(
     cors({
